37d61c2f9e756193befeb55516662aeb96027043
Steven Turner <steven.p.turner@proton.me>
2026-05-30 12:17:37 -0600

    (feat): add WriteJSON util function - writes status, message to json response

---
+++ utils/utils.go
@@ -0,0 +1,26 @@
+// Package utils
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Envelope map[string]interface{}
+
+func WriteJSON(w http.ResponseWriter, status int, data Envelope) error {
+	js, err := json.MarshalIndent(data, "", "")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(js)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
